4a53747ca621f94e0185b1d4f7b1bf3884f0450d
RedtigerAV <avvakhrushin@gmail.com>
2020-07-02 21:08:56 +0300

    DEPLOY ON HEROKU 2

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(compression());
 app.use(express.static(__dirname + '/dist/jira-timgo'));
 
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname + '/dist/jira-timgo/index.html')));
 
 if (devEnv) {
   app.use(errorHandler());
