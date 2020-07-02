5aa979761baa3218eea0e70dc911309189467bfc
RedtigerAV <avvakhrushin@gmail.com>
2020-07-02 23:30:51 +0300

    add fixes

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const ace = require('atlassian-connect-express');
-const config = require('atlassian-connect-express/lib/internal/config');
-const utils = require('atlassian-connect-express/lib/internal/utils');
 const express = require('express');
 const bodyParser = require('body-parser');
 const compression = require('compression');
@@ -9,16 +7,12 @@ const errorHandler = require('errorhandler');
 const path = require('path');
 const http = require('http');
 
-const CONFIG_FILENAME = 'config.json';
-const configOpts = utils.loadJSON(CONFIG_FILENAME);
-const configuration = config(configOpts);
-
 const app = express();
 const addon = ace(app);
+const port = addon.config.port();
 const devEnv = app.get('env') == 'development';
 
-app.set('disableHostCheck', true);
-app.set('port', configuration.port());
+app.set('port', port);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
@@ -33,10 +27,8 @@ if (devEnv) {
 
 app.use(addon.middleware());
 
-const server = http.createServer(app);
-
-server.listen(configuration.port());
-
-if (devEnv) {
-  addon.register();
-}
+http.createServer(app).listen(port, function(){
+  if (devEnv) {
+    addon.register();
+  }
+});
