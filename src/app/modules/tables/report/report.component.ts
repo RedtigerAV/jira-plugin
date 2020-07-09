07939798ff9258c42094abeecf34b21a4a3fbcde
RedtigerAV <avvakhrushin@gmail.com>
2020-07-09 08:47:44 +0300

    Create Default Settings Middleware

--- src/app/modules/tables/report/report.component.ts
+++ src/app/modules/tables/report/report.component.ts
@@ -1,23 +1,20 @@
 import { Component, OnInit, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
 import { IReportContext } from './interfaces/report-context.interfaces';
 import { ActivatedRoute } from '@angular/router';
-import { takeUntilDestroyed } from '@core/rxjs-operators/take-until-destroyed/take-until-destroyed.operator';
-import { SettingsPanelModalService } from '../../shared/settings-panel/settings-panel-modal/settings-panel-modal.service';
-import { TgSnackbarSuccess } from '@shared/components/tg-snackbar/models/tg-snackbar.models';
-import { TgSnackbarService } from '@shared/components/tg-snackbar/tg-snackbar.service';
+import { DefaultSettingsMiddleware } from '@core/services/default-settings.middleware';
 
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
   styleUrls: ['./report.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [DefaultSettingsMiddleware]
 })
 export class ReportComponent implements OnInit, OnDestroy {
   public readonly context: IReportContext;
 
   constructor(private readonly activatedRoute: ActivatedRoute,
-              private readonly snackbar: TgSnackbarService,
-              private readonly settingsPanelModalService: SettingsPanelModalService) {
+              private readonly defaultSettingsMiddleware: DefaultSettingsMiddleware) {
     this.context = this.activatedRoute.snapshot.data.context;
   }
 
@@ -29,10 +26,6 @@ export class ReportComponent implements OnInit, OnDestroy {
   }
 
   public onSettings(): void {
-    this.settingsPanelModalService.openDefaultSettingsPanelModel(this.context.tableID, this.context.settingsBuilder)
-      .pipe(takeUntilDestroyed(this))
-      .subscribe(() => {
-        this.snackbar.openSnackbar(new TgSnackbarSuccess('Настройки по умолчанию сохранены!'))
-      });
+    this.defaultSettingsMiddleware.openSettingsModalHandler(this.context.tableID, this.context.settingsBuilder);
   }
 }
